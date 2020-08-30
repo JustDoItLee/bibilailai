79d308092ac842d928ba5562e822fb6a53bfc940
judtdoitlee <657994434@qq.com>
2020-08-30 14:11:52 +0800

    n-queen

--- src/main/java/a/Backtrack.java
+++ src/main/java/a/Backtrack.java
@@ -36,7 +36,7 @@ public class Backtrack {
         return res;
     }
 
-    // 路径：记录在 track 中
+// 路径：记录在 track 中
 // 选择列表：nums 中不存在于 track 的那些元素
 // 结束条件：nums 中的元素全都在 track 中出现
     static void backtrack(int[] nums, LinkedList<Integer> track) {
--- src/main/java/a/Solution.java
+++ src/main/java/a/Solution.java
@@ -1,11 +1,12 @@
 package a;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  *
  */
 public class Solution {
-
+   
 }
