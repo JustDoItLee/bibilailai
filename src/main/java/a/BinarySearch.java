2696564757412133f24ce115704edf7f41526f05
judtdoitlee <657994434@qq.com>
2020-09-02 15:09:40 +0800

    binary search

--- src/main/java/a/BinarySearch.java
+++ src/main/java/a/BinarySearch.java
@@ -1,12 +1,14 @@
 package a;
 
+import java.util.Arrays;
+
 public class BinarySearch {
     /**
      * 因为我们初始化 right = nums.length - 1
      * 所以决定了我们的「搜索区间」是 [left, right]
      * 所以决定了 while (left <= right)
      * 同时也决定了 left = mid+1 和 right = mid-1
-     *
+     * <p>
      * 因为我们只需找到一个 target 的索引即可
      * 所以当 nums[mid] == target 时可以立即返回
      */
@@ -31,7 +33,7 @@ public class BinarySearch {
      * 所以决定了我们的「搜索区间」是 [left, right]
      * 所以决定了 while (left <= right)
      * 同时也决定了 left = mid + 1 和 right = mid - 1
-     *
+     * <p>
      * 因为我们需找到 target 的最左侧索引
      * 所以当 nums[mid] == target 时不要立即返回
      * 而要收紧右侧边界以锁定左侧边界
@@ -59,12 +61,11 @@ public class BinarySearch {
     }
 
     /**
-     *
      * 因为我们初始化 right = nums.length - 1
      * 所以决定了我们的「搜索区间」是 [left, right]
      * 所以决定了 while (left <= right)
      * 同时也决定了 left = mid + 1 和 right = mid - 1
-     *
+     * <p>
      * 因为我们需找到 target 的最右侧索引
      * 所以当 nums[mid] == target 时不要立即返回
      * 而要收紧左侧边界以锁定右侧边界
@@ -82,19 +83,24 @@ public class BinarySearch {
                 left = mid + 1;
             }
         }
-        // 这里改为检查 right 越界的情况，见下图
+        // 这里改为检查 right 越界的情况
         if (right < 0 || nums[right] != target)
             return -1;
         return right;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int left = leftBound(nums, target);
+        int right = rightBound(nums, target);
+        return new int[]{left, right};
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{-1, 0, 3, 5, 9, 12};
         int target = 9;
         System.out.println(new BinarySearch().search(arr, target));
-        int left = 50;
-        int right = 100;
-        System.out.println((left + right) / 2);
-        System.out.println(left + (right - left) / 2);
+        int[] arr2 = new int[]{5, 7, 7, 8, 8, 10};
+        int target2 = 8;
+        Arrays.stream(new BinarySearch().searchRange(arr2, target2)).forEach(x -> System.out.println(x));
     }
 }
