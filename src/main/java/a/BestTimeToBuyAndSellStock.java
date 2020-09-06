a6c06d92ac937d500b6856bb682012bf509449b0
judtdoitlee <657994434@qq.com>
2020-09-06 14:26:13 +0800

    gupiaomaimai

--- src/main/java/a/BestTimeToBuyAndSellStock.java
+++ src/main/java/a/BestTimeToBuyAndSellStock.java
@@ -1,23 +1,59 @@
 package a;
 
 /**
- *  给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
- *
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * <p>
  * 如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），设计一个算法来计算你所能获取的最大利润。
- *
+ * <p>
  * 注意：你不能在买入股票前卖出股票。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: [7,1,5,3,6,4]
  * 输出: 5
  * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
- *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+ * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+ * <p>
+ * 只允许一笔交易 k=1
+ * dp[i][1][0] = max(dp[i-1][1][0], dp[i-1][1][1] + prices[i])
+ * dp[i][1][1] = max(dp[i-1][1][1], dp[i-1][0][0] - prices[i])
+ *             = max(dp[i-1][1][1], -prices[i])
+ * k = 0 的 base case，所以 dp[i-1][0][0] = 0。
+ *
+ * 现在发现 k 都是 1，不会改变，即 k 对状态转移已经没有影响了。
+ * 可以进行进一步化简去掉所有 k：
+ * dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+ * dp[i][1] = max(dp[i-1][1], -prices[i])
  */
 public class BestTimeToBuyAndSellStock {
     public int maxProfit(int[] prices) {
-        return 0;
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i - 1 == -1) {
+                dp[i][0] = 0;
+                //   dp[i][0]
+                // = max(dp[-1][0], dp[-1][1] + prices[i])
+                // = max(0, -infinity + prices[i]) = 0
+                dp[i][1] = -prices[i];
+                //   dp[i][1]
+                // = max(dp[-1][1], dp[-1][0] - prices[i])
+                // = max(-infinity, 0 - prices[i])
+                // = -prices[i]
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BestTimeToBuyAndSellStock().maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
     }
 }
--- src/main/java/a/BestTimeToBuyAndSellStockII.java
+++ src/main/java/a/BestTimeToBuyAndSellStockII.java
@@ -2,19 +2,45 @@ package a;
 
 /**
  * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
- *
+ * <p>
  * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
- *
+ * <p>
  * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: [7,1,5,3,6,4]
  * 输出: 7
  * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
  *      随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+ * <p>
+ * k = infinity
+ * dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+ * dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
+ *             = max(dp[i-1][k][1], dp[i-1][k][0] - prices[i])
+ *
+ * 发现数组中的 k 已经不会改变了，也就是说不需要记录 k 这个状态了：
+ * dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+ * dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
  */
 public class BestTimeToBuyAndSellStockII {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n == 0) {
+            return 0;
+        }
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
+        }
+        return dp_i_0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BestTimeToBuyAndSellStockII().maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
 }
