e63958e3623de765cb77c380b4da5f895b5ed115
judtdoitlee <657994434@qq.com>
2020-09-20 14:58:47 +0800

    xxx

---
+++ src/main/java/a/LongestIncreasingSubsequence.java
@@ -0,0 +1,44 @@
+package a;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个无序的整数数组，找到其中最长上升子序列的长度。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [10,9,2,5,3,7,101,18]
+ * 输出: 4
+ * 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+ * 说明:
+ * <p>
+ * 可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+ * 你算法的时间复杂度应该为 O(n2) 。
+ * 进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+ */
+public class LongestIncreasingSubsequence {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        //子序列最小为1
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+        }
+        int res = 0;
+        for (int i = 0; i < dp.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+//        return Arrays.stream(dp).max().getAsInt();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestIncreasingSubsequence().lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+        ;
+    }
+}
