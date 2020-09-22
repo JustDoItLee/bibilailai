89b570e09bab83583585b4b92a33c9e8e25eebc8
judtdoitlee <657994434@qq.com>
2020-09-22 13:24:18 +0800

    xxx

--- src/main/java/a/Solution.java
+++ src/main/java/a/Solution.java
@@ -3,11 +3,55 @@ package a;
 import java.util.*;
 
 /**
- *
+ * 给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ * <p>
+ * 注意:
+ * <p>
+ * 每个数组中的元素不会超过 100
+ * 数组的大小不会超过 200
+ * 示例 1:
+ * <p>
+ * 输入: [1, 5, 11, 5]
+ * <p>
+ * 输出: true
+ * <p>
+ * 解释: 数组可以分割成 [1, 5, 5] 和 [11].
+ *  
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: [1, 2, 3, 5]
+ * <p>
+ * 输出: false
+ * <p>
+ * 解释: 数组不能分割成两个元素和相等的子集.
  */
 public class Solution {
-    public void openLock(String[] deadends, String target) {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        // 和为奇数时，不可能划分成两个和相等的集合
+        if (sum % 2 != 0) return false;
+        int n = nums.length;
+        sum = sum / 2;
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+        Arrays.fill(dp, false);
+        // base case
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = true;
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (j - nums[i - 1] < 0) {
+                    // 背包容量不足，不能装入第 i 个物品
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    // 装入或不装入背包
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[n][sum];
     }
 
     public static void main(String[] args) {
