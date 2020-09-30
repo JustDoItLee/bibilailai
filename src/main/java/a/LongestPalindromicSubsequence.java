2fd0a13edf1c89469013fef00c9e254c1eaf67da
judtdoitlee <657994434@qq.com>
2020-09-30 12:54:53 +0800

    xxx

---
+++ src/main/java/a/LongestPalindromicSubsequence.java
@@ -0,0 +1,59 @@
+package a;
+
+/**
+ * 给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * 输入:
+ * <p>
+ * "babbab"
+ * 输出:
+ * <p>
+ * 4
+ * 一个可能的最长回文子序列为 "bbbb"。
+ * <p>
+ * 示例 2:
+ * 输入:
+ * <p>
+ * "cbbd"
+ * 输出:
+ * <p>
+ * 2
+ * 一个可能的最长回文子序列为 "bb"。
+ * <p>
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 1000
+ * s 只包含小写英文字母
+ */
+public class LongestPalindromicSubsequence {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        // dp 数组全部初始化为 0
+        int[][] dp = new int[n][n];
+        // base case
+        for (int i = 0; i < n; i++)
+            dp[i][i] = 1;
+        // 反着遍历保证正确的状态转移
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                // 状态转移方程
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        // 整个 s 的最长回文子串长度
+        return dp[0][n - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestPalindromicSubsequence().longestPalindromeSubseq("babbab"));
+    }
+}
