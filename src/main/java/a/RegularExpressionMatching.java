33498d9146d451c43f9c9125f4416ac1bdcdeda0
judtdoitlee <657994434@qq.com>
2020-10-12 12:58:48 +0800

    xxx

---
+++ src/main/java/a/RegularExpressionMatching.java
@@ -0,0 +1,105 @@
+package a;
+
+import java.util.HashMap;
+
+/**
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ * <p>
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ * <p>
+ * 说明:
+ * <p>
+ * s 可能为空，且只包含从 a-z 的小写字母。
+ * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * s = "aa"
+ * p = "a"
+ * 输出: false
+ * 解释: "a" 无法匹配 "aa" 整个字符串。
+ * 示例 2:
+ * <p>
+ * 输入:
+ * s = "aa"
+ * p = "a*"
+ * 输出: true
+ * 解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+ * 示例 3:
+ * <p>
+ * 输入:
+ * s = "ab"
+ * p = ".*"
+ * 输出: true
+ * 解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+ * 示例 4:
+ * <p>
+ * 输入:
+ * s = "aab"
+ * p = "c*a*b"
+ * 输出: true
+ * 解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+ * 示例 5:
+ * <p>
+ * 输入:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * 输出: false
+ */
+public class RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        return dp(s, 0, p, 0);
+    }
+
+    HashMap<String, Boolean> memo = new HashMap<>();
+
+    /* 计算 p[j..] 是否匹配 s[i..] */
+    boolean dp(String s, int i, String p, int j) {
+        int m = s.length(), n = p.length();
+        // base case
+        if (j == n) {
+            return i == m;
+        }
+        if (i == m) {
+            if ((n - j) % 2 == 1) {
+                return false;
+            }
+            for (; j + 1 < n; j += 2) {
+                if (p.charAt(j + 1) != '*') {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // 记录状态 (i, j)，消除重叠子问题
+        String key = i + "," + j;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        boolean res = false;
+
+        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
+            if (j < n - 1 && p.charAt(j + 1) == '*') {
+                res = dp(s, i, p, j + 2)
+                        || dp(s, i + 1, p, j);
+            } else {
+                res = dp(s, i + 1, p, j + 1);
+            }
+        } else {
+            if (j < n - 1 && p.charAt(j + 1) == '*') {
+                res = dp(s, i, p, j + 2);
+            } else {
+                res = false;
+            }
+        }
+        // 将当前结果记入备忘录
+        memo.put(key, res);
+
+        return res;
+    }
+
+}
